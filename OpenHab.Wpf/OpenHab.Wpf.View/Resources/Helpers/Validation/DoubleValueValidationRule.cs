6261cf22af1303ad12d28e2ee50d95d01a7d51f7
Joerg Flechsig <goergchen@gmail.com>
2017-08-10 19:36:29 +0200

    implemented ControlTemplate for WidgetType Number in ItemControl

---
+++ OpenHab.Wpf/OpenHab.Wpf.View/Resources/Helpers/Validation/DoubleValueValidationRule.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace OpenHab.Wpf.View.Resources.Helpers.Validation
+{
+
+    public class DoubleValueValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var validationResult = new ValidationResult(true, null);
+
+            if (value == null) return new ValidationResult(false, Properties.Resources.FieldMustNotBeEmpty);
+            var success = double.TryParse(value.ToString(), out double _);
+
+            return !success ? new ValidationResult(false, Properties.Resources.IllegalCharacters) : validationResult;
+        }
+    }
+}
